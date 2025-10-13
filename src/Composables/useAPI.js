83df3a62c710efce63c3532b82ac795366bcd84c
Saul Hernandez <shern400@stu.southtexascollege.edu>
2025-10-12 22:20:26 -0500

    API CALL

---
+++ src/Composables/useAPI.js
@@ -0,0 +1,20 @@
+import axios
+imnport { ref }    from "vue";
+
+const instance = axios.create({
+  baseURL: "https://opentdb.com/",
+});
+
+const categories = ref([]);
+
+export function useAPI() {
+    const getCategories = async () => {
+        if (categories.value.length === 0) {
+            const response = await instance.get("api_category.php");
+            categories.value = response.data.trivia_categories;
+        }
+    }
+
+     return { instance, categories, getCategories }
+} 
+
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,8 +1,20 @@
 <script setup>
-import { ref } from 'vue'
-import BaseTitle from '@/components/BaseTitle.vue'
+import { onMounted, ref } from 'vue'
+import BaseTitle from '@/components/BaseTitle.vue';
+import { useAPI } from '@/Composables/useAPI'
+
+const { categories, getCategories } = useAPI()
+
+onMounted(async () => {
+  await getCategories()
+  console.log(categories)
+})    
+
 </script>
 
 <template>
   <BaseTitle>TRIVIA APPLICATION</BaseTitle>
+  <div v-for="category in categories" :key=""category.id">
+    {{ category.name }}
+  </div>
 </template>
